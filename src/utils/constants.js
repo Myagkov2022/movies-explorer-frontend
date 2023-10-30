ae2321a3ea0f364615c4a1ce23463b7fbc0c3c82
Ivan Myagkov <myagkovivan2022@yandex.ru>
2023-10-30 20:47:47 +0300

    level-3

---
+++ src/utils/constants.js
@@ -0,0 +1,14 @@
+export const SCREEN_SM = 550;
+export const SCREEN_MD = 900;
+export const SCREEN_LG = 1280;
+
+export const MOVIES_CARDS_1280 = 12;
+export const MOVIES_CARDS_900 = 8;
+export const MOVIES_CARDS_550 = 5;
+
+export const ADD_MOVIES_CARD_1280 = 3;
+export const ADD_MOVIES_CARD_900 = 2;
+export const ADD_MOVIES_CARD_550 = 2;
+
+
+export const SHORTS_MOVIES= 41;
